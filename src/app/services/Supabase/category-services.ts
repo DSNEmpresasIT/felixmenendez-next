a556f15cdb64b17037314c6c8e6a2a253fbe8446
DanielMino19 <danielmino926@gmail.com>
2023-11-28 15:11:18 -0300

    the error message was modified

--- src/app/services/Supabase/category-services.ts
+++ src/app/services/Supabase/category-services.ts
@@ -1,5 +1,3 @@
-//TODO: 1.INSTALLAR SUPABASE, 2.TRAER LAS CATEGORIAS, 3. HACER UN MAP DE LAS CATEGORIAS EN EL HEADER PRODUCTOS.
-
 import { Category } from "@/app/util/types";
 import { createClient } from "@supabase/supabase-js";
 
@@ -7,7 +5,7 @@ const MAIN_SUPABASE_URL = process.env.MAIN_SUPABASE_URL;
 const MAIN_SUPABASE_KEY = process.env.MAIN_SUPABASE_KEY;
 
 if (!MAIN_SUPABASE_URL || !MAIN_SUPABASE_KEY) {
-  throw new Error('The SUPABASE_URL and SUPABASE_KEY environment variables are mandatory in categories service.');
+  throw new Error('The SUPABASE_URL and SUPABASE_KEY environment variables are mandatory in categories.');
 }
 
 const supabase = createClient(MAIN_SUPABASE_URL, MAIN_SUPABASE_KEY);
